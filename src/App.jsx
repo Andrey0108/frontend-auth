4bd9a432e35fcb23dbdfee547c7f747674a467d8
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-04-11 09:46:27 -0500

    install axios for services

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // El formulario debe ir en login.jsx (todo lo que hay en app por ahora)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(username + " " + password);
+  };
 
   return (
     <>
-      <h1>Auth</h1>
+      <form onSubmit={handleSubmit}>
+        <h1>Auth</h1>
+        <label>
+          <input
+            type="text"
+            value={username}
+            name="Usuario"
+            placeholder="Usuario"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            value={password}
+            name="Contraseña"
+            placeholder="Contraseña"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+        <input type="submit" value="Iniciar sesión" />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
